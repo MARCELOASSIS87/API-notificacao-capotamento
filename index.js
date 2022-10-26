cbd23362fb84fec3fffeb501592db7062874f36e
Marcelo Assis <Marceloassis50@gmail.com>
2022-10-26 14:53:36 -0300

    removi o listen

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ server.get('https://api-notificacao-capotamento.vercel.app/',(req,res) => {
     return res.json({mensagem: 'Api está funfa'}) 
 })
 
-server.listen(3000,() => {
-    console.log('Server em ação funcionando')
-})
+// server.listen(3000,() => {
+//     console.log('Server em ação funcionando')
+// })
