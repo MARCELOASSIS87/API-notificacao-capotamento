42364b3a0543485c86def31454d21fb44623a91f
Marcelo Assis <Marceloassis50@gmail.com>
2022-10-26 14:50:27 -0300

    teste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
 
 server.get('/',(req,res) => {
-    return res.json({mensagem: 'Api está funcionando teste'}) 
+    return res.json({mensagem: 'Api está funcionando'}) 
 })
 
 server.listen(3000,() => {
