74421e6efa938501c8bdbb89ef78af087682cb9b
Marcelo Assis <Marceloassis50@gmail.com>
2022-10-26 14:52:04 -0300

    teste2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const server = express()
 
-server.get('https://api-notificacao-capotamento.vercel.app//',(req,res) => {
+server.get('https://api-notificacao-capotamento.vercel.app/',(req,res) => {
     return res.json({mensagem: 'Api está funcionando'}) 
 })
 
